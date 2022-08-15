a18ba5fc0b080e64f577e6aaee7e1750af5fe8ec
crane3216 <turumi3216@gmail.com>
2022-08-15 14:29:29 +0900

    修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,15 @@
 class Item < ApplicationRecord
-  belongs_to :user
+  # belongs_to :user
 
-  with_options presence: true do
-    validates :name
-    validates :info
-    validates :price
-    validates :sales_status_id
-    validates :schedule_delivery_id
-    validates :prefecture_id
-    validates :category_id
-    validates :shipping_fee_status_id
-    validates :user
-  end
+  # with_options presence: true do
+  #   validates :name
+  #   validates :info
+  #   validates :price
+  #   validates :sales_status_id
+  #   validates :schedule_delivery_id
+  #   validates :prefecture_id
+  #   validates :category_id
+  #   validates :shipping_fee_status_id
+  #   validates :user
+  # end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              { 'furima tarou' }
     email                 { Faker::Internet.free_email }
-    password              { Faker::Internet.password(min_length: 6) }
+    password              { '1a' + Faker::Internet.password(min_length: 4) }
     password_confirmation { password }
     last_name             { '佐藤' }
     first_name            { '太郎' }
