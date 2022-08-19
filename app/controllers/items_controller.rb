169a5eb33863c59d9206fb5d086cd2dda8b0c09e
crane3216 <turumi3216@gmail.com>
2022-08-19 21:21:26 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: :index
 
   def index
-    @items = Item.all.order('created_at DESC')
+    # @items = Item.all.order('created_at DESC')
   end
 
   def new
@@ -18,10 +18,10 @@ class ItemsController < ApplicationController
     end
   end
 
-  def destroy
-    @item.destroy
-    redirect_to root_path
-  end
+  # def destroy
+  #   @item.destroy
+  #   redirect_to root_path
+  # end
 
   private
 
