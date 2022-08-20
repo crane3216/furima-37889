a62f12df10bf803f0b80f34fb0a033f6e273ceea
crane3216 <turumi3216@gmail.com>
2022-08-20 21:06:57 +0900

    修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,6 +12,7 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name, length: { minimum: 1, maxmum: 40 }
     validates :info, length: { minimum: 1, maxmum: 1000 }
+    validates :price
     validates :sales_status_id
     validates :schedule_delivery_id
     validates :prefecture_id
@@ -21,8 +22,9 @@ class Item < ApplicationRecord
     validates :image
   end
 
-  validates :price, presence: true, format: { with: /\A[0-9]+\z/ },
-  numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  with_options numericality: { only_integer: true, greater_than: 299, less_than: 10_000_000 } do
+    validates :price, format: { with: /\A-?[0-9]+(\.[0-9]+)?\z/, message: '半角数字で入力' }
+  end
 
   with_options numericality: { other_than: 1 } do
     validates :category_id
